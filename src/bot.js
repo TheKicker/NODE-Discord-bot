413c2c7a9efb7eaabc144f8cd1f24489d57299ce
The Kicker <cav.lemasters@geneva.edu>
2022-08-25 13:50:50 -0400

    Restructuring

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,42 @@
 require('dotenv').config();
 const tok = process.env.DISCORD_BOT_TOKEN
 
-const { Client, GatewayIntentBits } = require('discord.js')
-const sigma = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-
-
-sigma.login(tok)
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js')
+const sigma = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent] });
 
 sigma.on("ready", ()=>{
-    console.log("Sigma just landed")
+
+    // random activity 
+    r = Math.floor(Math.random() * 4)
+    if(r === 0){
+        sigma.user.setActivity('Yellowstone', { type: ActivityType.Watching });
+    } else if (r===1){
+        sigma.user.setActivity('Spotify', { type: ActivityType.Listening });
+    } else if (r===2){
+        sigma.user.setActivity('Chess', { type: ActivityType.Competing });
+    } else {
+        sigma.user.setActivity('Rainbow 6 Siege', { type: ActivityType.Playing });
+    }
+    
+    console.log(`${sigma.user.tag} just landed! R${r}`)
 })
+
+sigma.on('messageCreate', msg => {
+    // Prevent the bot from responding to itself and potentially becoming sentient
+    if(msg.author.bot){return}
+
+    // Take the content from the user, and make it lower case
+    m = msg.content.toLowerCase()
+
+    if(m === "hello"){
+        msg.channel.send(`Hello to you too, ${msg.author}`)
+    }
+    
+    if(m.startsWith("beans")){
+        msg.react("😺")
+        msg.reply("Beans da cat")
+    }
+
+});
+
+sigma.login(tok)

8d2f003e9f4389ea6ac5257c5a2fa83939aef6f0
The Kicker <cav.lemasters@geneva.edu>
2022-08-25 10:29:46 -0400

    Base project created

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
-
 const tok = process.env.DISCORD_BOT_TOKEN
 
-console.log(tok)
+const { Client, GatewayIntentBits } = require('discord.js')
+const sigma = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+
+sigma.login(tok)
+
+sigma.on("ready", ()=>{
+    console.log("Sigma just landed")
+})

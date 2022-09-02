56da9fd81c563710a7e1b6491e72e7c466aaabc6
The Kicker <cav.lemasters@geneva.edu>
2022-09-02 14:58:58 -0400

    Rebuild, installed OpenAIGPT

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const tok = process.env.DISCORD_BOT_TOKEN
 
-const { Client, GatewayIntentBits, ActivityType, ModalSubmitFields } = require('discord.js')
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js')
 const sigma = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent] });
+
+// External modules
+var greetings = require('./etc/linguistics').greeting;
+var fruits = require('./etc/linguistics').fruits;
+var positive = require('./etc/linguistics').positive;
+var negative = require('./etc/linguistics').negative;
 
 sigma.on("ready", ()=>{
     // random activity 
@@ -28,14 +34,34 @@ sigma.on('messageCreate', msg => {
     // Take the content from the user, and make it lower case
     m = msg.content.toLowerCase()
 
-    if(m === "hello"){
+    if(greetings.includes(m)){
         msg.channel.sendTyping()
         setTimeout(()=>{ msg.channel.send(`Hello to you ${msg.author.username}`) }, Math.floor(Math.random() * 10000))
+    }
+
+    if(positive.includes(m)){
+        msg.channel.sendTyping()
+        setTimeout(()=>{ msg.channel.send(`Glad to hear it ${msg.author.username}, tell me more!`) }, Math.floor(Math.random() * 10000))
+    }
+
+    if(negative.includes(m)){
+        msg.channel.sendTyping()
+        setTimeout(()=>{ msg.channel.send(`Hmm, ${msg.author.username}, what don't you like about it?`) }, Math.floor(Math.random() * 10000))
     }
     
     if(m.startsWith("beans")){
         msg.react("😺")
         msg.reply("Beans da cat")
+    }
+
+    if(m.includes("trump")){
+        msg.channel.sendTyping()
+        setTimeout(()=>{ msg.channel.send(`https://media4.giphy.com/media/l2JhIUyUs8KDCCf3W/giphy.gif?cid=ecf05e47xwjph9stu2nmq0b86z5lqx43234zotlfl64cflz5&rid=giphy.gif&ct=g`) }, Math.floor(Math.random() * 10000))
+    }
+
+    if(m.includes("repeat")){
+        msg.channel.sendTyping()
+        setTimeout(()=>{ msg.channel.send(`According to ${msg.author}, ${m}`) }, Math.floor(Math.random() * 10000))
     }
 
 });
---
+++ src/etc/linguistics.js
@@ -0,0 +1,9 @@
+greeting = ["hello", "hi", "howdy", "hey", "sup", "aye"]
+fruits = ["apple", "banana", "orange", "grape", "peaches", "strawberries", "blueberries", "kiwi", "plum"]
+positive = ["i like", "i love", "i don't hate", "i dont hate", "i really like", "i really love", "i want", "i need"]
+negative = ["i don't like", "i dont like", "i hate", "i really dont like", "i really don't like", "i really hate", "i dont", "i don't want", "i dont need", "i don't need"]
+
+module.exports.greeting = greeting
+module.exports.fruits = fruits
+module.exports.positive = positive
+module.exports.negative = negative
